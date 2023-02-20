a8e439c6a645dbfa1892e51c0b04e6fa4a5d87bf
Jesse1211 <lzhliu@ucdavis.edu>
2023-02-19 19:26:26 -0800

    build web2

---
+++ Web2/src/test/java/SpringFramework/Web2/Web2ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringFramework.Web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
