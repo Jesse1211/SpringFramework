cc68f3aa2dc13704bac76b258277b21beb98caf7
Jesse1211 <lzhliu@ucdavis.edu>
2023-04-10 14:43:54 -0700

    Print Data into corresponded pages. Closes #4

--- springPetClinic/PetClinicWeb/src/main/java/springFramework/springPetClinic/controller/OwnerController.java
+++ springPetClinic/PetClinicWeb/src/main/java/springFramework/springPetClinic/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package springFramework.springPetClinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springFramework.springPetClinic.services.IOwner;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final IOwner ownerService;
 
+    public OwnerController(IOwner ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    // Spring MVC calls the function with injecting a model which is returned by Thymeleaf
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        // addAttribute(property name inside model, the set to be iterated over)
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
--- springPetClinic/PetClinicWeb/src/main/java/springFramework/springPetClinic/controller/VetController.java
+++ springPetClinic/PetClinicWeb/src/main/java/springFramework/springPetClinic/controller/VetController.java
@@ -1,13 +1,19 @@
 package springFramework.springPetClinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springFramework.springPetClinic.services.IVet;
 
 @Controller
 public class VetController {
-
+    private final IVet vetService;
+    public VetController(IVet vetService) {
+        this.vetService = vetService;
+    }
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
