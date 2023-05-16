8cd85440ebfca0807b2a2fdd112c26fe7b5b5095
Jesse1211 <lzhliu@ucdavis.edu>
2023-05-16 10:54:57 -0700

    update request URL

--- springPetClinic/PetClinicWeb/src/main/java/springFramework/springPetClinic/controller/IndexController.java
+++ springPetClinic/PetClinicWeb/src/main/java/springFramework/springPetClinic/controller/IndexController.java
@@ -12,7 +12,7 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping("/ops")
+    @RequestMapping("/oups")
     public String opsHandler(){
         return "notImplemented";
     }
