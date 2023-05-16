03b55a4660ff5c60f073655f82caf6cb9df55b7a
Jesse1211 <lzhliu@ucdavis.edu>
2023-05-16 11:25:13 -0700

    Update Owner with JPA entity. Issue #11.

--- springPetClinic/PetClinicData/src/main/java/springFramework/springPetClinic/model/Owner.java
+++ springPetClinic/PetClinicData/src/main/java/springFramework/springPetClinic/model/Owner.java
@@ -1,14 +1,24 @@
 package springFramework.springPetClinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>(); // init for null exception
 
     // contact info property
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
     public Set<Pet> getPets() {
