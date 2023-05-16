340ee9f3405a81888cb479667c95e151345418db
Jesse1211 <lzhliu@ucdavis.edu>
2023-05-16 11:40:13 -0700

    Update PetType with JPA entity. Issue #11.

--- springPetClinic/PetClinicData/src/main/java/springFramework/springPetClinic/model/PetType.java
+++ springPetClinic/PetClinicData/src/main/java/springFramework/springPetClinic/model/PetType.java
@@ -1,7 +1,12 @@
 package springFramework.springPetClinic.model;
 
-public class PetType extends BaseEntity{
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
